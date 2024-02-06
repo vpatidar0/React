a8ed6870d8a8de9334dc12899fd6ed94f3b194b9
vikash <vikas181056@gmail.com>
2024-02-06 12:53:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const parent=React.createElement('div',{id:'parent'},
+    [React.createElement('div',{id:'child'},
+    [React.createElement('h1',{},'Hello i am h1 tag'),React.createElement('h2',{},'Hello i am h2 tag')]),
+    React.createElement('div',{id:'child2'},
+    [React.createElement('h3',{},'Hello i am h3 tag'),React.createElement('h4',{},'Hello i am h4 tag')])])
+
+const heading =React.createElement('h1',{id:'heading'},'Hello world')
+const root =ReactDOM.createRoot(document.getElementById('root'))
+console.log(parent)
+root.render(parent);
