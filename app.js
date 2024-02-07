edcaa3132e2feac780debfba4faba15b965a825a
vikash <vikas181056@gmail.com>
2024-02-07 11:55:32 +0530

    react setup

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
 const parent=React.createElement('div',{id:'parent'},
     [React.createElement('div',{id:'child'},
     [React.createElement('h1',{},'Hello i am h1 tag'),React.createElement('h2',{},'Hello i am h2 tag')]),
@@ -5,6 +7,5 @@ const parent=React.createElement('div',{id:'parent'},
     [React.createElement('h3',{},'Hello i am h3 tag'),React.createElement('h4',{},'Hello i am h4 tag')])])
 
 const heading =React.createElement('h1',{id:'heading'},'Hello world')
-const root =ReactDOM.createRoot(document.getElementById('root'))
-console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(parent);
